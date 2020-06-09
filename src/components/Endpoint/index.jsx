a3ba01ea96c64a5d47234b0e59844d713aac1a80
Alessio Fimognari <alessiofimognari@AML0220.local>
2020-06-09 11:45:30 +0100

    changed baseUrl to /

--- src/components/Endpoint/index.jsx
+++ src/components/Endpoint/index.jsx
@@ -22,7 +22,7 @@ const isUp = async (url, isHtml, interval) => {
   if (url) {
     try {
       const res = await axios.get(
-        `http://localhost:3333${
+        `/${
           isHtml ? "/html" : "/api"
         }?url=${url}&ttl=${interval}`,
         { timeout: (interval || DEFAULT_INTERVAL) * 1000 + 10 } // adding 10ms to make sure server-side ends first
